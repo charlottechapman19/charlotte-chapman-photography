206c19470f036352d6f95215390bae33e4e2bdad
Craig McNicholas <craig.mcnicholas@yotta.co.uk>
2020-09-12 14:41:45 +0100

    adding cara & steve, elinor & dan and will & ed weddings

--- _script.js
+++ _script.js
@@ -1,26 +1,43 @@
 var fs = require('fs');
- 
+
 if (process.argv.length <= 4) {
-	console.log("Usage: " + __filename + " path/to/directory path/to/output folder-name");
-	process.exit(-1);
+  console.log(
+    'Usage: ' + __filename + ' path/to/directory path/to/output folder-name',
+  );
+  process.exit(-1);
 }
- 
+
 var path = process.argv[2];
 var output = process.argv[3];
 var folderName = process.argv[4];
+var titlePrefix = process.argv[5] || '';
 var html = '';
- 
-fs.readdir(path, function(err, items) {
-	for (var i = 0; i < items.length; i++) {
-		console.log(items[i]);
-		html = html + '<div class="col-xs-12">\n' +
-			'<img class="img-responsive" src="' + folderName + '/' + items[i] + '" alt="Charlotte Chapman Photography" title="Charlotte Chapman Photography" />\n' +
-			'</div>\n\n';
-    }
+
+// format the title
+titlePrefix =
+  (titlePrefix ? titlePrefix + ' - ' : '') + 'Charlotte Chapman Photography';
+
+fs.readdir(path, function (err, items) {
+  for (var i = 0; i < items.length; i++) {
+    console.log(items[i]);
+    html =
+      html +
+      '<div class="col-xs-12">\n' +
+      '<img class="img-responsive" src="' +
+      folderName +
+      '/' +
+      items[i] +
+      '" alt="' +
+      titlePrefix +
+      '" title="' +
+      titlePrefix +
+      '" />\n' +
+      '</div>\n\n';
+  }
 });
 
 var stream = fs.createWriteStream(output);
-stream.once('open', function(fd) {
+stream.once('open', function (fd) {
   stream.write(html);
   stream.end();
 });
